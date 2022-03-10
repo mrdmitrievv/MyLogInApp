17ee034439486dc0dba39e0126a46a3797b5b848
mrdmitrievv <mrdmitrievv@yandex.ru>
2022-03-10 16:30:29 +0300

    Initial Commit

---
+++ MyLogInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLogInApp
+//
+//  Created by Артём Дмитриев on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyLogInApp/WelcomeUserViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class WelcomeUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
