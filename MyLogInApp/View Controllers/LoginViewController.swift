0904b17ca149e8f47a25b91265f0a890f9722458
mrdmitrievv <mrdmitrievv@yandex.ru>
2022-03-11 21:15:52 +0300

    Added showAlert func and alerts to forgot login and password buttons

--- MyLogInApp/View Controllers/LoginViewController.swift
+++ MyLogInApp/View Controllers/LoginViewController.swift
@@ -1,22 +1,45 @@
-//
-//  ViewController.swift
-//  MyLogInApp
-//
-//  Created by Артём Дмитриев on 10.03.2022.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    let user = User.getUserData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func unwindSegue(_ sender: UIStoryboardSegue) {
-        print("The unwiding segue")        
+        loginTextField.text = ""
+        passwordTextField.text = ""
     }
-
+    
+    @IBAction func forgotUserNameButton() {
+        showAlert(
+            title: "Oops",
+            message: "Just remember, your login is \(user.login)"
+        )
+    }
+    
+    @IBAction func forgotPasswordButton() {
+        showAlert(
+            title: "Oops",
+            message: "Just remember, your password is \(user.password)"
+        )
+    }
+    
+    
+    
 }
 
+extension LoginViewController {
+    func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in textField?.text = nil }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
